47d3e73b9b973bfbc6f9350c023aafd40541b9bf
unknown <brosjayme@gmail.com>
2023-08-11 10:04:59 +0100

    making an api call

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
   //  console.log('render')
   useEffect(() => {
     // console.log("resource type changed");
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => setItems(json));
   }, [resourceType]);
   return (
     <>
@@ -17,6 +18,9 @@ function App() {
         <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </>
   );
 }
